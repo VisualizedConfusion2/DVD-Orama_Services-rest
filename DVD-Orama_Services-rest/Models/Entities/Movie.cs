50c2481a7fff85802eb197a42d3e8af78820c836
PatrickJarshoej <paj003@edu.zealand.dk>
2026-04-30 10:11:03 +0200

    Created movie model and updated movie collection model

---
+++ DVD-Orama_Services-rest/Models/Entities/Movie.cs
@@ -0,0 +1,18 @@
+﻿namespace DVD_Orama_Services_rest.Models.Entities
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int Duration { get; set; } //stored in seconds
+        public int PublicationYear { get; set; } 
+        public List<string> Genres { get; set; } //Could be an enum, i just don't feel like writing out that many genres
+        public List<string> Actors { get; set; } //In a more complete edition of this product you might have an actor class
+        public List<string> StreamingServices { get; set; } //Could also have been an enum
+        public string Barcode { get; set; } = string.Empty;
+
+
+
+    }
+}
--- DVD-Orama_Services-rest/Models/Entities/MovieCollection.cs
+++ DVD-Orama_Services-rest/Models/Entities/MovieCollection.cs
@@ -3,10 +3,15 @@
     public class MovieCollectionItem
     {
         public int Id { get; set; }
-        public string Barcode { get; set; } = string.Empty;
+        //public string Barcode { get; set; } = string.Empty;
 
-        public int UserId { get; set; }
-        public User User { get; set; } = null!;
+        //public int UserId { get; set; }
+        public User Owner { get; set; } = null!;
+        public bool IsPublic { get; set; } = false;
+        public List<Movie> Movies { get; set; }
+        public List<int> AdminIds { get; set; }
+        public List<int> RegUserIds { get; set; }
+        public List<int> ViewerIds { get; set; }
 
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
